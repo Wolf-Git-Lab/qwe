b787caffa9d0ca63bcf66976c45cafca534c1cd3
Wolf-Git-Lab <mixa.15112000@yandex.ru>
2021-10-21 21:17:33 +0300

    add Project

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.Json;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person tom = new Person { Name = "Tom", Age = 35 };
+            string json = JsonSerializer.Serialize<Person>(tom);
+            Console.WriteLine(json);
+            Console.ReadKey();
+        }
+    }
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
